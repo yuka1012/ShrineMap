b7792b6a632403302643ddf9e6478374355ab695
松本優香 <matsumotoyuka@matsumotoyuukanoMacBook-Pro.local>
2018-07-21 08:44:06 +0900

    ユーザのエンティティクラス作成
    q

--- app/models/User.java
+++ app/models/User.java
@@ -2,6 +2,7 @@ package models;
 
 import java.util.Date;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Version;
@@ -9,6 +10,9 @@ import javax.validation.constraints.NotNull;
 
 import com.avaje.ebean.Model;
 import com.avaje.ebean.annotation.CreatedTimestamp;
+import com.avaje.ebean.annotation.UpdatedTimestamp;
+
+import play.data.validation.Constraints.Email;
 
 /**
  * 会員情報を保持するエンティティクラス.
@@ -18,21 +22,33 @@ import com.avaje.ebean.annotation.CreatedTimestamp;
 @Entity
 public class User extends Model{
 
+	/* 主キー*/
 	@Id
 	private Long id;
 	
+	/* ユーザ名*/
 	@NotNull
+	@Column(unique=true)
 	private String userNm;
 	
+	/* メールアドレス*/
 	@NotNull
+	@Email
 	private String email;
 	
+	/* パスワード*/
+	@NotNull
+	private String password;
+	
+	/* 登録日*/
 	@CreatedTimestamp
 	private Date createDate;
 	
-	@Version
+	/* 更新日*/
+	@UpdatedTimestamp
 	private Date updateDate;
 	
+	/* 削除フラグ*/
 	@NotNull
 	private Integer delFlg;
 	
