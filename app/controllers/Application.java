8fbbd211e1bcb654aa1eb07c289755cfb0e00448
松本優香 <matsumotoyuka@matsumotoyuukanoMacBook-Pro.local>
2018-08-10 17:17:00 +0900

    ユーザ登録画面新規作成

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -6,6 +6,7 @@ import org.slf4j.LoggerFactory;
 import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.index;
+import views.html.makeNewUser;
 
 public class Application extends Controller{
 	private static final Logger logger = LoggerFactory.getLogger(Application.class);
@@ -13,5 +14,10 @@ public class Application extends Controller{
 	public Result index() {
 		logger.info("Application#top");
 		return ok(index.render("神社まっぷ","ようこそ社の森へ"));
+	}
+	
+	public Result makeNewUser() {
+		logger.info("Application#makeNewUser");
+		return ok(makeNewUser.render("神社まっぷ","ようこそ社の森へ"));
 	}
 }
